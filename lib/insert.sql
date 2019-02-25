5ea6ddf405d9d2fe9d683bf5e03803b04889719b
Ariel <arielord@buffalo.edu>
2019-02-25 16:54:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Mr.Chocolate, 2, male, brown, docile, true);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Rowdy, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Tabitha, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Sergeant Brown, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Melissa, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Grinch, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Wendy, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(NULL, NULL, NULL, NULL, NULL, NULL);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Mr.Chocolate, 2, male, brown, docile, true);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Rowdy, NULL, NULL, NULL, NULL, NULL);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Tabitha, NULL, NULL, NULL, NULL, NULL);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Sergeant Brown, NULL, NULL, NULL, NULL, NULL);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Melissa, NULL, NULL, NULL, NULL, NULL);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Grinch, NULL, NULL, NULL, NULL, NULL);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(Wendy, NULL, NULL, NULL, NULL, NULL);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(NULL, NULL, NULL, NULL, NULL, NULL);
