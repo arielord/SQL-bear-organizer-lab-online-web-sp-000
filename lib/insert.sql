75f737a9557e79fd41ea1f9f9ab6e9519eed9abb
Ariel <arielord@buffalo.edu>
2019-02-25 16:47:11 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(NULL, Mr.Chocolate, NULL, NULL, NULL, NULL, NULL)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(NULL, Rowdy, NULL, NULL, NULL, NULL, NULL)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(NULL, Tabitha, NULL, NULL, NULL, NULL, NULL)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(NULL, Sergeant Brown, NULL, NULL, NULL, NULL, NULL)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(NULL, Melissa, NULL, NULL, NULL, NULL, NULL)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(NULL, Grinch, NULL, NULL, NULL, NULL, NULL)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(NULL, Wendy, NULL, NULL, NULL, NULL, NULL)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES(NULL, NULL, NULL, NULL, NULL, NULL, NULL)
